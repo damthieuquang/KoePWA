d8ebb09f8401f51534866380e1ee58b67585542c
quangdt <damthieuquang@gmail.com>
2019-10-09 15:17:58 +0700

    update

--- public/public/scripts/app.js
+++ public/public/scripts/app.js
@@ -163,21 +163,7 @@ function getForecastFromNetwork(coords) {
  */
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
-  if (!('caches' in window)) {
-    return null;
-  }
-  const url = `${window.location.origin}/forecast/${coords}`;
-  return caches.match(url)
-      .then((response) => {
-        if (response) {
-          return response.json();
-        }
-        return null;
-      })
-      .catch((err) => {
-        console.error('Error getting data from cache', err);
-        return null;
-      });
+
 }
 
 /**
@@ -212,10 +198,6 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
-    getForecastFromCache(location.geo)
-    .then((forecast) => {
-      renderForecast(card, forecast);
-    });
 
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
--- public/public/scripts/install.js
+++ public/public/scripts/install.js
@@ -22,7 +22,7 @@ const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
 // CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
 
 /**
  * Event handler for beforeinstallprompt event.
@@ -32,8 +32,7 @@ window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
  */
 function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
-  deferredInstallPrompt = evt;
-  installButton.removeAttribute('hidden');
+
 }
 
 
@@ -44,23 +43,13 @@ function saveBeforeInstallPromptEvent(evt) {
  */
 function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
-  deferredInstallPrompt.prompt();
-  // Hide the install button, it can't be called twice.
-	evt.srcElement.setAttribute('hidden', true);
+
   // CODELAB: Log user response to prompt.
-	deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
-    });
+
 }
 
 // CODELAB: Add event listener for appinstalled event
-window.addEventListener('appinstalled', logAppInstalled);
+
 /**
  * Event handler for appinstalled event.
  *   Log the installation to analytics or save the event somehow.
@@ -69,5 +58,5 @@ window.addEventListener('appinstalled', logAppInstalled);
  */
 function logAppInstalled(evt) {
   // CODELAB: Add code to log the event
-  console.log('Weather App was installed.', evt);
+
 }
